539a43d8c422fae8ac4e03f8b8b1a3292f1d5ac3
A701\1dam <vicaragoz@alu.edu.gva.es>
2024-09-30 21:03:40 +0200

    Practica 3 comentada para aclarar que hace cada parte

--- untitled/src/Practica3.java
+++ untitled/src/Practica3.java
@@ -2,9 +2,9 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Practica3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //creacion de clase
 
-        int edad = 0;
+        int edad = 0; //predefinir edad para las excepciones
         System.out.println("Como te llamas?");
 
         Scanner entrada = new Scanner(System.in);
@@ -16,12 +16,12 @@ public class Practica3 {
                 System.out.println("Hola " + name + ". Introduce tu edad:");
                 edad = entrada.nextInt();
 
-                if (edad <= 0){
+                if (edad <= 0){ //Excepcion para no poner numeros negativos
                     System.out.println("No puedes tener 0 años o menos");
                 }
 
             } catch (NumberFormatException | InputMismatchException | ArithmeticException e) {
-                System.out.println("ERROR: introduce un numero valido ");
+                System.out.println("ERROR: introduce un numero valido "); //Error si no se introduce un numero
                 entrada.nextLine();
             }
         }
@@ -29,8 +29,8 @@ public class Practica3 {
         if (edad >= 18) {
             System.out.println("Enhorabuena " + name + ". Puedes votar.");
         } else {
-            System.out.println("Lo siento " + name + ". No puedes votar.");
-            int rest = 18 - edad;
+            System.out.println("Lo siento " + name + ". No puedes votar.");  //Salida
+            int rest = 18 - edad; //Años restantes para votar
             System.out.println("Te faltan " + rest + " años para ser mayor de edad.");
         }
     }
